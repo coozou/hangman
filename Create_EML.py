7253db22206016f5081ff8707e3c6298ee4065b8
coozou <50235732+coozou@users.noreply.github.com>
2019-05-17 12:09:38 +0900

    Add files via upload

---
+++ Create_EML.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 16 21:59:12 2019
+
+@author: shinozakiryo
+"""
+
+import email
+from email.mime.text import MIMEText
+from email.utils import formataddr
+from email.header import Header
+from email import generator
+
+
+
+
+
+from_addr = 'coozou0911@gmail.com'
+to_addr = 'coozou0001@icloud.com,coozou0002@icloud.com,coozou0003@icloud.com'
+subject = "御礼"
+body = """お世話になっております。
+本日はありがとうございました。
+今後とも宜しくお願い致します。"""
+
+encoding = 'utf-8'
+sender_name = Header('Ryo Shinozaki', encoding).encode()
+
+message = MIMEText(body.encode(encoding), 'plain', _charset=encoding)
+message['Subject'] = Header(subject, encoding)
+message['From'] = formataddr((sender_name, from_addr))
+message['To'] = to_addr
+message.add_header('X-Unsent', '1')
+
+with open('test.eml', 'w') as eml:
+    gen = generator.Generator(eml)
+    gen.flatten(message)
+
+print("** end **")
